e6a897cf94a731628922a49c606f30489178e83e
László Király <laszlo.t.kiraly@gmail.com>
2017-12-08 23:47:03 +0100

    day 8 part 1

---
+++ day8.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const FILENAME = 'day8.input';
+
+/**
+ * reads the input
+ * @return {string[]} the input data
+ */
+function readInput() {
+  const inputFile = fs.readFileSync(FILENAME, {encoding: 'utf-8'});
+  const inputData = [];
+  for (let line of inputFile.split('\n')) {
+    inputData.push(line);
+  }
+  return inputData;
+}
+
+/**
+ * reads the input
+ * @param {string[]} lines as the input
+ * @return {number} number of characters in all lines
+ */
+function solve(lines) {
+  let countChars = 0;
+  const alphaRegex = /[a-zA-Z]+/g;
+  for (let line of lines) {
+    let lineChars = 0;
+    for (let i=1, j=line.length; i < j - 1; i++) {
+      if (line[i].match(alphaRegex)) {
+        lineChars++;
+      } else if (line[i] == '\\') {
+        if (line[i+1] == '"' || line[i+1] == '\\') {
+          i++;
+          lineChars++;
+        } else if (line[i+1] == 'x') {
+          i += 3;
+          lineChars++;
+        }
+      }
+    }
+    countChars += line.length - lineChars;
+    console.log(`${line}: length=${line.length},chars=${lineChars}`);
+  }
+  return countChars;
+}
+console.log(solve(readInput()));
+
+console.log('Doner');
