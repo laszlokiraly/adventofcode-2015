fd2afa28e10862c56ec89da72dd0114744165ce4
László Király <laszlo.t.kiraly@gmail.com>
2017-12-08 23:46:38 +0100

    eslint and update node to 6.12.2

--- day7.js
+++ day7.js
@@ -1,90 +1,96 @@
-var fs = require('fs');
-var input = fs.readFileSync('day7.input', {encoding: 'utf-8'});
+const fs = require('fs');
+const input = fs.readFileSync('day7.input', {encoding: 'utf-8'});
 
-var assignment_regex = /^([0-9]+|[a-z]+) -> ([a-z]+)$/g;
-var and_regex = /^([0-9]+|[a-z]+) (?:AND) ([a-z]+) -> ([a-z]+)$/g;
-var or_regex = /^([a-z]+) (?:OR) ([a-z]+) -> ([a-z]+)$/g;
-var rshift_regex = /^([a-z]+) (?:RSHIFT) ([0-9]+) -> ([a-z]+)$/g;
-var lshift_regex = /^([a-z]+) (?:LSHIFT) ([0-9]+) -> ([a-z]+)$/g;
-var not_regex = /^(?:NOT) ([a-z]+) -> ([a-z]+)$/g;
+const assignmentRegex = /^([0-9]+|[a-z]+) -> ([a-z]+)$/g;
+const andRegex = /^([0-9]+|[a-z]+) (?:AND) ([a-z]+) -> ([a-z]+)$/g;
+const orRegex = /^([a-z]+) (?:OR) ([a-z]+) -> ([a-z]+)$/g;
+const rshiftRegex = /^([a-z]+) (?:RSHIFT) ([0-9]+) -> ([a-z]+)$/g;
+const lshiftRegex = /^([a-z]+) (?:LSHIFT) ([0-9]+) -> ([a-z]+)$/g;
+const notRegex = /^(?:NOT) ([a-z]+) -> ([a-z]+)$/g;
 
-var memory = new Map();
+const memory = new Map();
 
+/**
+ * reads the input
+ */
 function readInput() {
-  memory = new Map()
-  for (var line of input.split('\n')) {
+  memory = new Map();
+  for (let line of input.split('\n')) {
     if (line != '') {
-      var variable_regex = new RegExp(/^.* -> ([a-z]+)$/g)
-      memory.set(variable_regex.exec(line)[1], line)
+      let variableRegex = new RegExp(/^.* -> ([a-z]+)$/g);
+      memory.set(variableRegex.exec(line)[1], line);
     }
   }
 }
-readInput()
+readInput();
 
-var goal = 'a'
-
+constgoal = 'a';
+/**
+ * @param {string} variable name
+ * @return {string} the assignment
+ */
 function calcValue(variable) {
-  var line = memory.get(variable)
-  if (line.match(assignment_regex)) {
-    var regexp = new RegExp(assignment_regex);
-    var matches = regexp.exec(line);
+  let line = memory.get(variable);
+  if (line.match(assignmentRegex)) {
+    let regexp = new RegExp(assignmentRegex);
+    let matches = regexp.exec(line);
     if (isNaN(matches[1])) {
-      var value = calcValue(matches[1])
-      memory.set(variable, `${value} -> ${variable}`)
-      return value
+      let value = calcValue(matches[1]);
+      memory.set(variable, `${value} -> ${variable}`);
+      return value;
     } else {
-      return matches[1]
+      return matches[1];
     }
-  } else if (line.match(and_regex)) {
-    var regexp = new RegExp(and_regex);
-    var matches = regexp.exec(line);
+  } else if (line.match(andRegex)) {
+    let regexp = new RegExp(andRegex);
+    let matches = regexp.exec(line);
     if (isNaN(matches[1])) {
-      var value = calcValue(matches[1]) & calcValue(matches[2])
-      memory.set(variable, `${value} -> ${variable}`)
-      return value
+      let value = calcValue(matches[1]) & calcValue(matches[2]);
+      memory.set(variable, `${value} -> ${variable}`);
+      return value;
     } else {
-      var value = matches[1] & calcValue(matches[2])
-      memory.set(variable, `${value} -> ${variable}`)
-      return value
+      let value = matches[1] & calcValue(matches[2]);
+      memory.set(variable, `${value} -> ${variable}`);
+      return value;
     }
-  } else if (line.match(or_regex)) {
-    var regexp = new RegExp(or_regex);
-    var matches = regexp.exec(line);
+  } else if (line.match(orRegex)) {
+    let regexp = new RegExp(orRegex);
+    let matches = regexp.exec(line);
     if (isNaN(matches[1])) {
-      var value = calcValue(matches[1]) | calcValue(matches[2])
-      memory.set(variable, `${value} -> ${variable}`)
-      return value
+      let value = calcValue(matches[1]) | calcValue(matches[2]);
+      memory.set(variable, `${value} -> ${variable}`);
+      return value;
     } else {
-      var value = matches[1] | calcValue(matches[2])
-      memory.set(variable, `${value} -> ${variable}`)
-      return value
+      let value = matches[1] | calcValue(matches[2]);
+      memory.set(variable, `${value} -> ${variable}`);
+      return value;
     }
-  } else if (line.match(not_regex)) {
-    var regexp = new RegExp(not_regex);
-    var matches = regexp.exec(line);
-    var value = ~ calcValue(matches[1])
-    memory.set(variable, `${value} -> ${variable}`)
-    return value
-  } else if (line.match(lshift_regex)) {
-    var regexp = new RegExp(lshift_regex);
-    var matches = regexp.exec(line);
-    var value = calcValue(matches[1]) << matches[2]
-    memory.set(variable, `${value} -> ${variable}`)
-    return value
-  } else if (line.match(rshift_regex)) {
-    var regexp = new RegExp(rshift_regex);
-    var matches = regexp.exec(line);
-    var value = calcValue(matches[1]) >> matches[2]
-    memory.set(variable, `${value} -> ${variable}`)
-    return value
+  } else if (line.match(notRegex)) {
+    let regexp = new RegExp(notRegex);
+    let matches = regexp.exec(line);
+    let value = ~ calcValue(matches[1]);
+    memory.set(variable, `${value} -> ${variable}`);
+    return value;
+  } else if (line.match(lshiftRegex)) {
+    let regexp = new RegExp(lshiftRegex);
+    let matches = regexp.exec(line);
+    let value = calcValue(matches[1]) << matches[2];
+    memory.set(variable, `${value} -> ${variable}`);
+    return value;
+  } else if (line.match(rshiftRegex)) {
+    let regexp = new RegExp(rshiftRegex);
+    let matches = regexp.exec(line);
+    let value = calcValue(matches[1]) >> matches[2];
+    memory.set(variable, `${value} -> ${variable}`);
+    return value;
   }
 }
 
-var solution1 = calcValue(goal)
-console.log(`part 1: value of ${goal} = ` + solution1)
+const solution1 = calcValue(goal);
+console.log(`part 1: value of ${goal} = ` + solution1);
 
 
 // part 2
-readInput()
-memory.set('b', `${solution1} -> b`)
-console.log(`part 2: value of ${goal} = ` + calcValue(goal))
+readInput();
+memory.set('b', `${solution1} -> b`);
+console.log(`part 2: value of ${goal} = ` + calcValue(goal));
