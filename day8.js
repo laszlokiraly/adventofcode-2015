ea14b11ef33b04b29237a57838b34307024e6a9f
László Király <laszlo.t.kiraly@gmail.com>
2017-12-09 00:12:38 +0100

    day 8 part 1 and 2

--- day8.js
+++ day8.js
@@ -15,32 +15,50 @@ function readInput() {
 }
 
 /**
- * solves day 8 part 1
+ * solves day 8 part 2
  * @param {string[]} lines as the input
- * @return {number} number of characters in all lines
+ * @return {number[]} number of characters in all lines
  */
 function solve(lines) {
-  let countChars = 0;
+  let countCharsPart1 = 0;
+  let countCharsPart2 = 0;
   const alphaRegex = /[a-zA-Z]+/g;
   for (let line of lines) {
+    if (line == '') {
+      continue;
+    }
     let lineChars = 0;
+    let newLine = '"\\"';
+    let newLineChars = 3;
     for (let i=1, j=line.length; i < j - 1; i++) {
       if (line[i].match(alphaRegex)) {
         lineChars++;
+        newLine += line[i];
+        newLineChars++;
       } else if (line[i] == '\\') {
+        newLine += line[i] + line[i];
+        newLineChars += 2;
         if (line[i+1] == '"' || line[i+1] == '\\') {
+          newLine += line[i] + line[i+1];
+          newLineChars += 2;
           i++;
           lineChars++;
         } else if (line[i+1] == 'x') {
+          newLine += line[i+1] + line[i+2] + line[i+3];
+          newLineChars += 3;
           i += 3;
           lineChars++;
         }
       }
     }
-    countChars += line.length - lineChars;
-    console.log(`${line}: length=${line.length},chars=${lineChars}`);
+    newLine += '\\""';
+    newLineChars += 3;
+    countCharsPart1 += line.length - lineChars;
+    countCharsPart2 += newLine.length - line.length;
+    // console.log(`${line}: length=${line.length},chars=${lineChars}`);
+    // console.log(`${newLine}: length=${newLine.length},chars=${newLineChars}`);
   }
-  return countChars;
+  return [`part 1: ${countCharsPart1}`, `part 2: ${countCharsPart2}`];
 }
 console.log(solve(readInput()));
 
